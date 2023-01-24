b680dbb5b080ef1b6cac39018a0c763bcd9d43ad
satou1024 <taku.satou.sys22@morijyobi.ac.jp>
2023-01-24 14:40:58 +0900

    一覧表示を実装しました

---
+++ src/main/java/dao/DVDDAO.java
@@ -0,0 +1,60 @@
+package dao;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import dto.DVD;
+
+public class DVDDAO {
+	private static Connection getConnection() throws URISyntaxException, SQLException {
+		try {
+			Class.forName("org.postgresql.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	    URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+	    String username = dbUri.getUserInfo().split(":")[0];
+	    String password = dbUri.getUserInfo().split(":")[1];
+	    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+	    return DriverManager.getConnection(dbUrl, username, password);
+	}
+	public static List<DVD> selectAllBooks() {
+	
+		List<DVD> result = new ArrayList<>();
+
+		String sql = "SELECT * FROM DVD";
+		
+		try (
+				Connection con = getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql);
+				){
+			try (ResultSet rs = pstmt.executeQuery()){
+				while(rs.next()) {
+					int id = rs.getInt("id");
+					String name=rs.getString("name");
+					String directed_by = rs.getString("directed_by");
+					int number = rs.getInt("number");
+					int rental = rs.getInt("rental");
+
+					DVD employee=new DVD(id,name,directed_by,number,rental);
+					
+					result.add(employee);
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+		return result;
+		}
+	}
+	
---
+++ src/main/webapp/WEB-INF/view/list.jsp
@@ -0,0 +1,39 @@
+
+<%@ page import="java.util.List" %>
+<%@ page import="java.util.ArrayList" %>
+
+<%@ page import="dto.DVD" %>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>一覧表示</title>
+</head>
+<body>
+<h1>商品一覧</h1>
+	<table border="1">
+		<tr>
+			<th>ID</th>
+			<th>名前</th>
+			<th>監督名</th>
+			<th>商品番号</th>
+			<th>貸し出し</th>
+		</tr>
+	<%
+	List<DVD> list = (ArrayList<DVD>)request.getAttribute("list");
+	for(DVD s : list) {
+	%>
+		<tr>
+			<td><%=s.getId() %></td>
+			<td><a href="detailServlet"><%=s.getName() %></a></td>
+			<td><%=s.getDirected_by() %></td>
+			<td><%=s.getNumber() %></td>
+			<td><%=s.getRental() %>
+		</tr>
+	<%} %>
+	</table>
+
+</body>
+</html>
